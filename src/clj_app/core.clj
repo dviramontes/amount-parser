9a7e1c581772cb3d471c95e3569a52b87937bb68
david viramontes <dviramontes@gmail.com>
2014-11-20 20:51:59 -0500

    adds digit-parser

--- src/clj_app/core.clj
+++ src/clj_app/core.clj
@@ -7,14 +7,34 @@
   (println "Hello, World!"))
 
 
-(defn count-tens [n] (int (/ n 10)))
+(defn count-tens [n]
+  (int (/ n 10)))
 
-(defn count-hundreds [n] (int (/ n 100)))
+(defn count-hundreds [n]
+  (int (/ n 100)))
 
-(defn rational [n] (if (rational? n) n (float (rationalize n))))
+(defn get-n-after-decimal-point [n]
+  " Get .00 digits,
+    up to 2 decimal places? :("
+  (int
+   (* ;; make it a whole number
+     (read-string ;; make it a number again
+       (format "0%s"
+               (first (rest (re-find #"\d+(\.\d{1,2})?" (str n)))))) 100)))
+
+(get-n-after-decimal-point 2.10) ;; => 10
+(get-n-after-decimal-point 2.01) ;; => 1
+
+(defn digit-parser [x & [power]]
+  (let [p (or power 10)]
+  (rem x (or power 10))))
+
+(digit-parser 213 100)
 
 (defn amount [n]
+
   "Accepts an amount and convert it to the appropriate string Dollar representation."
+
   (let [ zero-to-nineteen
                  {0 "zero"
                   1 "one",  2 "two",  3 "three",  4 "four",
@@ -24,24 +44,30 @@
                   16 "sixteen", 17 "sevente", 18 "eighteen",
                   19 "nineteen"}
 
-         tens {20 "twenty", 30 "thirty", 40 "fourty", 50 "fifty", 60 "sixty" , 70 "seventy", 80 "eighty", 90 "ninety"}
+         tens {20 "twenty", 30 "thirty", 40 "fourty", 50 "fifty",
+               60 "sixty" , 70 "seventy", 80 "eighty", 90 "ninety"}
 
 
-         ;; assumes rational numbers
-         answer
-           (cond (false? (number? n)) :not-a-number
-                 (<= n 19) (zero-to-nineteen (if (integer? n) n (int n)))
-                 (and (zero? (mod n 10)) (>= 20) (<= 100)) (tens (* (count-tens n) 10))
-
-
-                 :else "out of range")]
+         ;; look for rational numbers if not then ...
+         answer (if (not (number? n)) :not-a-number
+                 (if (rational? n)
+                   ((fn [rational]
+                      (cond
+                       (<= rational 19) (zero-to-nineteen rational)
+                       (and (zero? (mod n 10)) (>= 20) (<= 99)) (tens (* (count-tens n) 10))
+                       (and (zero? (mod n 100))(>= 100) (<= 999)) (tens (* (count-tens n) 100))
+                      )) n)
+                   ((fn [irrational]
+                      :irrational
+                       ) n)))]
 
       (println answer)
 
-      (when (not= :not-a-number answer)
+      (when (not= answer :not-a-number)
         (str
-          (format "%s dollars :: %d" answer n)))
-    ))
+          (format "%s dollars :: %d" answer n)))))
 
-(amount 30)
+(amount 3)
+
+
 
