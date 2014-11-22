882f1b4ccc4f94e25e4eee9997792fec5ab193dd
david viramontes <dviramontes@gmail.com>
2014-11-22 01:50:58 -0500

    adds test

--- src/clj_app/core.clj
+++ src/clj_app/core.clj
@@ -18,18 +18,19 @@
 (def tens     {20 "twenty", 30 "thirty",  40 "fourty", 50 "fifty",
                60 "sixty" , 70 "seventy", 80 "eighty", 90 "ninety"})
 
-(defn count-tens [n]
+(defn decimate-tens [n]
   (* (int (/ n 10)) 10))
 
-(defn count-hundreds [n]
+(decimate-tens 22)
+ (defn decimate-hundreds [n]
   (int (/ n 100)))
-
-(count-hundreds 100)
 
 (defn get-n-after-decimal-point [n]
 
   " Get .00 digits,
-    up to 2 decimal places? :("
+    up to 2 decimal places :/
+    sorry for the regex, taken
+    from SO"
 
   (int ;; make it a whole number
    (*
@@ -37,59 +38,55 @@
        (format "0%s"
                (first (rest (re-find #"\d+(\.\d{1,2})?" (str n)))))) 100)))
 
-(get-n-after-decimal-point 2.10) ;; => 10
-(get-n-after-decimal-point 2.01) ;; => 1
-
-
-(defn single-digit-parser [x & [power]]
-  (let [p (or power 10)]
-  (rem x p)))
-
-(single-digit-parser 122 100)
+(defn single-digit-parser [n & [product]]
+  (let [p (or product 10)]
+  (rem n p)))
 
 (defn final-tens-formater [n] ;; naming is hard ? :?
-  (str (tens (count-tens n)) "-" (zero-to-nineteen (single-digit-parser n))))
+  (str (tens (decimate-tens n)) "-" (zero-to-nineteen (single-digit-parser n))))
 
 (defn final-hundreds-formater [n f]
   (if (zero? (mod n 100))
-    (str (zero-to-nineteen (count-hundreds n)) " hundred dollars")
-    (str (zero-to-nineteen (count-hundreds n)) " hundred and " (f (single-digit-parser n 100)))))
+    (str (zero-to-nineteen (decimate-hundreds n)) " hundred dollars")
+    (str (zero-to-nineteen (decimate-hundreds n)) " hundred and " (f (single-digit-parser n 100)))))
 
+(defn final-cents-formatter [n]
+  (str (final-tens-formater (get-n-after-decimal-point n))))
 
 (defn amount [n]
 
   "Accepts an amount and convert it to the appropriate string Dollar representation."
 
-  (let [
-         ;; look for rational numbers if not then ...
+  (let [ ;; look for rational numbers
          answer (if (not (number? n)) :not-a-number
                  (if (rational? n)
                    ((fn [r] ;; rational numbers
                       (let
                         [tens-fiter (and (>= r 20) (<= r 99))
-                         hundreds-filter (and (>= r 100) (<= r 999))])
+                         hundreds-filter (and (>= r 100) (<= r 999))
+                         thousands-filter (and (>= r 100) (<= r 999))
+                         ])
                         (cond
                          (<= r 19) (zero-to-nineteen r)
-                         (and (zero? (mod n 10)) 'tens-fiter)  (tens (count-tens n))
-                         (and (zero? (mod n 100)) 'hundreds-filter) (tens (count-tens n))
+                         (and (zero? (mod n 10)) 'tens-fiter)  (tens (decimate-tens n))
+                         (and (zero? (mod n 100)) 'hundreds-filter) (tens (decimate-tens n))
                          '(tens-fiter) (final-tens-formater r)
                          '(hundreds-filter) (final-hundreds-formater r amount)
                         )) n)
                     ((fn [i] ;; irrational numbers
-                       :irrational
-                       ) n)))]
+                       (if
+                        (zero? i) nil
+
+                        )) n)))]
 
       (println answer)
-
+      ;; side-effecting
       (when (not= answer :not-a-number)
         (if (= answer "one")
         (str (format "%s dollar :: %d" answer n)) ;; the singular case
         (str (format "%s dollars :: %d" answer n)))))) ;; the plural case
 
-(amount 200)
 
-(final-tens-formater 23)
 
-(final-hundreds-formater 455 amount)
 
 
--- test/clj_app/core_test.clj
+++ test/clj_app/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [clj-app.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest decimate-tens-function-test
+  (testing "Given 25 should result in 20"
+    (is (= (decimate-tens 25) 20) "some message if failed")))
+
+(deftest decimate-hundreds-function-test
+  (testing "Given 250 should result in 2"
+    (is (= (decimate-hundreds 250) 2) "some message if failed")))
+
+(deftest get-n-after-decimal-point-test
+  (testing "Given 2.01 and 2.10,
+            should result in 1 and 10 respectively"
+    (is (= (get-n-after-decimal-point 2.01) 1))
+    (is (= (get-n-after-decimal-point 2.10) 10))))
