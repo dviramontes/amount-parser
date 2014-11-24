d2551693917dba784f6a5ad4d14c65931d0ad54a
david viramontes <dviramontes@gmail.com>
2014-11-23 23:34:07 -0700

    refactors amount function, begins thousand functions

--- src/clj_app/core.clj
+++ src/clj_app/core.clj
@@ -12,17 +12,20 @@
    5 "five", 6 "six",  7 "seven",  8 "eight",
    9 "nine", 10 "ten", 11 "eleven",12 "twelve",
    13 "thirteen", 14 "fourteen",   15 "fifteen",
-   16 "sixteen", 17 "sevente", 18 "eighteen",
+   16 "sixteen", 17 "seventeen", 18 "eighteen",
    19 "nineteen"})
 
-(def tens     {20 "twenty", 30 "thirty",  40 "fourty", 50 "fifty",
-               60 "sixty" , 70 "seventy", 80 "eighty", 90 "ninety"})
+(def tens   {20 "twenty", 30 "thirty",  40 "fourty", 50 "fifty",
+             60 "sixty" , 70 "seventy", 80 "eighty", 90 "ninety"})
 
 (defn decimate-tens [n]
   (* (int (/ n 10)) 10))
 
 (defn decimate-hundreds [n]
   (int (/ n 100)))
+
+(defn decimate-thousands [n]
+  (int (/ n 1000)))
 
 (defn get-n-after-decimal-point [n]
 
@@ -32,62 +35,48 @@
   from SO"
 
   (int ;; make it a whole number
-   (*
-    (read-string ;; make it a number again
-     (format "0%s"
-             (first (rest (re-find #"\d+(\.\d{1,2})?" (str n)))))) 100)))
+   (* (read-string ;; make it a number again
+       (format "0%s" (first (rest (re-find #"\d+(\.\d{1,2})?" (str n))))))
+      100)))
 
 (defn single-digit-parser [n & [product]]
   (let [p (or product 10)]
     (rem n p)))
 
-(defn final-tens-formater [n]
-  (str (tens (decimate-tens n)) "-" (zero-to-nineteen (single-digit-parser n))))
+(defn tens-filter [n]
+  (and (>= n 20) (<= n 99)))
 
-(defn final-hundreds-formater [n fun]
-  (if (zero? (mod n 100))
-  (str (zero-to-nineteen (decimate-hundreds n)) " hundred dollars")
-    (str (zero-to-nineteen (decimate-hundreds n)) " hundred and " (fun (single-digit-parser n 100)))))
+(defn hundreds-filter [n]
+  (and (>= n 100)(<= n 999)))
 
-(defn final-cents-formatter [n]
-  (str (final-tens-formater (get-n-after-decimal-point n))))
+(defn thousands-filter [n]
+  (and (>= n 1000) (<= n 9999)))
 
-(final-tens-formater 20)
+(defn filter-amount [n]
+  (cond
+   (not (number? n)) :not-a-number
+   (rational? n)(do
+                  (cond
+                   (<= n 19) (zero-to-nineteen n)
+                   (and (zero? (mod n 10))  (tens-filter n)) (tens (decimate-tens n))
+                   (and (zero? (mod n 100)) (hundreds-filter n)) (str (zero-to-nineteen (decimate-hundreds n)) " hundred")
+                   (and (zero? (mod n 100)) (thousands-filter n)) (str (zero-to-nineteen (decimate-thousands n)) " thousand")
+                   (tens-filter n)(str (tens (decimate-tens n)) "-" (zero-to-nineteen (single-digit-parser n)))
+                   (hundreds-filter n) (str (zero-to-nineteen (decimate-hundreds n)) " hundred and " (filter-amount (single-digit-parser n 100)))
+                   (thousands-filter n) (str (zero-to-nineteen (decimate-hundreds n)) " hundred and " (filter-amount (single-digit-parser n 100)))))
+   (not (rational? n))()))
 
-(defn amount [n]
+
+(defn amount [x]
 
   "Accepts an amount and convert it to the appropriate string Dollar representation."
 
-  (let [ ;; look for rational numbers
-        answer (if (not (number? n)) :not-a-number
-                 (if (rational? n)
-                   ((fn [r] ;; rational numbers
-                      (let
-                        [tens-fiter (and (>= r 20) (<= r 99))
-                         hundreds-filter (and (>= r 100) (<= r 999))
-                         thousands-filter (and (>= r 100) (<= r 999))])
-                      (cond
-                       (<= r 19) (zero-to-nineteen r)
-                       (and (zero? (mod n 10)) 'tens-fiter)  (tens (decimate-tens n))
-                       (and (zero? (mod n 100)) 'hundreds-filter) (tens (decimate-tens n))
-                       'tens-fiter (final-tens-formater r)
-                       'hundreds-filter (final-hundreds-formater r amount))) n)
-                   ((fn [i] ;; irrational numbers
-                      (if
-                        (zero? i) nil
-                        ()
-                        )) n)))]
-
-    (println answer)
-    ;; side-effecting?
-    (when (not= answer :not-a-number)
-      (if (= answer "one")
-        (str (format "%s dollar" answer)) ;; the singular case
-        (str (format "%s dollars" answer)))))) ;; the plural case
+    (let [answer (filter-amount x)]
+    (do
+      (when (not= answer :not-a-number)
+        (if (= answer "one")
+          (format "%s dollar" answer)        ;; the singular case
+          (format "%s dollars" answer)))))) ;; the plural case
 
 
-(amount 1)
-
-(final-tens-formater 22)
-(final-hundreds-formater 1 amount)
-(final-hundreds-formater 999 amount)
+(amount 9000)
--- test/clj_app/core_test.clj
+++ test/clj_app/core_test.clj
@@ -21,17 +21,23 @@
     given 25 should result in 5 for example"
     (is (= (single-digit-parser 25) 5))))
 
-(deftest final-tens-formater-test
-  (testing "Given 25, it should result in the
-    str twenty-five with dash"
-    (is (= (final-tens-formater 25) "twenty-five"))
-    (is (not (empty? (re-find #"-" (final-tens-formater 25)))))))
+;; (deftest final-tens-formater-test
+;;   (testing "Given 25, it should result in the
+;;     string twenty-five with dash"
+;;     (is (= (final-tens-formater 25) "twenty-five"))
+;;     (is (not (empty? (re-find #"-" (final-tens-formater 25)))))))
 
-(deftest final-hundreds-formater-test
-  (testing "Given 250, should result in
-    the str two hundred and fifty dollars"
-    (is (= (final-hundreds-formater 250 amount) "two hundred and fifty dollars"))))
+;; (deftest final-hundreds-formater-test
+;;   (testing "Given 250, should result in
+;;     the str two hundred and fifty dollars"
+;;     (is (= (final-hundreds-formater 250 amount) "two hundred and fifty dollars"))))
 
-(deftest amount-test
+(deftest rational-amount-test
   (testing "Given 1, should result in 1 [singular] dollar"
-    (is (= (amount 1) "one dollar"))))
+    (is (= (amount 1) "one dollar")))
+  (testing "Given a rational number 25,
+    should result in the string twenty-five dollars"
+    (is (= (amount 25) "twenty-five dollars")))
+  (testing "Given 100, should result in one hundred dollars"
+    (is (= (amount 100) "one hundred dollars"))))
+
