40acd7b4aef23fd43d1c24b2c011b4f896463777
dviramontes <dviramontes@gmail.com>
2015-03-01 10:24:11 -0700

    fixes input fn

--- src/cljs/amount_parser/core.cljs
+++ src/cljs/amount_parser/core.cljs
@@ -12,15 +12,22 @@
     {:value "One thousand two hundred and thirty-four dollars with 22/100 cents"}))
 
 (defn fetch-amount [n]
-  (let [callback (fn [new-data] #(swap! app-state assoc :value new-data))]
+  (let [callback (fn [new-data] (.log js/console new-data))]
   (.get js/$ (str "/amount/" n) callback)))
 
+(q/defcomponent input-amount []
+  (d/div {:className "col-lg"}
+         (d/input {:type "number" :id "formInput" 
+                   :className "form-control" :placeholder 1234.22
+                   :onChange (fn [x] (fetch-amount (.val (js/$ "#formInput"))))})))
+
+(q/defcomponent show-amount [amount]
+  (d/div {:className "col-lg-6"}
+      (input-amount)
+      (d/pre {:className "result"} amount)))
+
 (q/defcomponent main [value]
-  (d/div {:className "col-sm-3"}
-    (d/input {:className "form-control" :type "number" :id "formInput"
-      :placeholder 1234.22 :step "any" :size 15}))
-  (d/div {:className "col-sm-6"} 
-      (d/pre {:className "result"} (:lname value))))
+  (show-amount (:value value)))
       
-(q/render (main {:fname 123 :lname "viramontes"}) (.getElementById js/document "app"))
+(q/render (main @app-state) (.getElementById js/document "app"))
 
